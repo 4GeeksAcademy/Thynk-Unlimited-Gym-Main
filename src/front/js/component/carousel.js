0b97faccd842ff8d3b13d892ed9f227bab592207
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-03-12 23:41:08 +0000

    slider carousel

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -1,45 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../styles/testimonials.css";
 
 export const Carousel = () => {
-  const slides = document.querySelectorAll(".slide");
-  let index = 0;
-
-  function prevSlide() {
-    slides[index].classList.remove("active");
-    index--;
-
-    if (index < 0) index = slides.length - 1;
-
-    slides[index].classList.add("active");
-  }
-
-  document.querySelector(".prev").addEventListener("click", (e) => {
-    prevSlide();
-  });
-
-  function nextSlide() {
-    slides[index].classList.remove("active");
-    index++;
-
-    if (index > slides.length - 1) index = 0;
-
-    slides[index].classList.add("active");
-  }
-
-  document.querySelector(".next").addEventListener("click", (e) => {
-    nextSlide();
-  });
+  
 
   return (
-    <div>
-      <div id="slider">
-        <div className="slide">testimonials 1</div>
-        <div className="slide">2</div>
-        <div className="slide">3</div>
-      </div>
-      <button className="prev">Prev</button>
-      <button className="next">Next</button>
-    </div>
+    <div class="pic-ctn">
+    <img src="https://picsum.photos/200/300?t=1" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=2" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=3" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=4" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=5" alt="" class="pic"/>
+  </div>
   );
 };
