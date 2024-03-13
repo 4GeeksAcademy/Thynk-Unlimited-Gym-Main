b968d76994fa42266cbd95d1ea2f1d297d04a5f2
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-03-13 00:41:16 +0000

    added temporary carousel

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,17 +5,21 @@ import { Jumbotron } from "../component/Jumbotron";
 import { Carousel } from "../component/carousel";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-	const title= "Thynk Gym Unlimited"
-	const description = " Unleash Your Potential"
+  const { store, actions } = useContext(Context);
+  const title = "Thynk Gym Unlimited";
+  const description = " Unleash Your Potential";
 
-
-	return (
-		<div className="text-center mt-5">
-			<div> <Jumbotron title ={title} description = {description}/> </div>
-			<div><Carousel/></div>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <div>
+        {" "}
+        <Jumbotron title={title} description={description} />{" "}
+      </div>
+      <div>
+        <Carousel />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
