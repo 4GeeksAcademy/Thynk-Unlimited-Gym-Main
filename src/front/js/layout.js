46995f2f33d09bc0fcba7d02c2ea11f6eeb9e97f
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-03-15 16:31:47 +0000

    added hard-coded schedule

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,36 +11,37 @@ import { Footer } from "./component/footer";
 import injectContext from "./store/appContext";
 import { MembershipViewPage } from "./pages/membershipView";
 import { Navbar } from "./component/navbar";
+import { Classes } from "./pages/eventScheduler";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
+    return <BackendURL />;
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        {/* <Route element={<Member />} path="/member" /> */}
-                        <Route element={<Single />} path="/single/:theid" />
- Resources-MO
-                        <Route element={<Resources />} path="/resources" />
-
-                        <Route element={< MembershipViewPage/> } path="/membershipView/:id"/> 
-main
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Resources />} path="/resources" />
+            <Route
+              element={<MembershipViewPage />}
+              path="/membershipView/:id"
+            />
+            <Route element={<Classes />} path="/classes" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
