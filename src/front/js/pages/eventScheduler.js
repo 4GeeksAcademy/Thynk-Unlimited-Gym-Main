b3edbd9db36e7c655d96762609497b01d3c274a1
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-03-16 16:48:08 +0000

    updating classes to be more dynamic

--- src/front/js/pages/eventScheduler.js
+++ src/front/js/pages/eventScheduler.js
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/classes.css";
 
 export const Classes = () => {
+
+  const timeSlots = [
+    "6:00am - 8:00am",
+    "10:00am - 12:00am",
+    "5:00pm - 7:00pm",
+    "7:00pm - 9:00pm",
+  ];
+
+  const workouts = [
+    { name: "Pilates", trainer: "Jenna Smith" },
+    { name: "Cardio", trainer: "Jenna Smith" },
+    { name: "Yoga", trainer: "Marcus Chen" },
+    { name: "Circuits", trainer: "Alexis Taylor" },
+    { name: "Boxing", trainer: "Ryan Johnson" },
+    { name: "Body Building", trainer: "Robert Cage" },
+    { name: "Karate", trainer: "Samantha Rodriguez" },
+  ];
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const handleSchedule = (day, timeSlot) => {
+    if (
+      schedule.hasOwnProperty(day) &&
+      schedule[day].hasOwnProperty(timeSlot)
+    ) {
+      let workout = schedule[day][timeSlot];
+      return {
+        workout: workouts.name,
+        trainer: workouts.trainer,
+        day: day,
+        timeSlot: timeSlot,
+      };
+    } else {
+      return {
+        workout: "",
+        trainer: "",
+        day: day,
+        timeSlot: timeSlot,
+      };
+    }
+  };
+
+  const schedule = {};
+  schedule["Sunday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "5:00pm - 7:00pm": workouts[3],
+    "7:00pm - 9:00pm": workouts[4],
+  },
+  schedule["Monday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1],
+    "7:00pm - 9:00pm": workouts[4],
+  };
+  schedule["Tuesday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1],
+    "7:00pm - 9:00pm": workouts[4],
+  };
+  schedule["Wednesday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1],
+    "7:00pm - 9:00pm": workouts[4],
+  };
+  schedule["Thursday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1],
+    "7:00pm - 9:00pm": workouts[4],
+  };
+  schedule["Friday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1],
+    "7:00pm - 9:00pm": workouts[4],
+  };
+  schedule["Saturday"] = {
+    "6:00am - 8:00am": workouts[0],
+    "10:00am - 12:00am": workouts[3],
+    "5:00pm - 7:00pm": workouts[1]
+  };
 
   return (
     <section className="class-timetable-section spad">
@@ -20,207 +111,26 @@ export const Classes = () => {
                 <thead>
                   <tr>
                     <th></th>
-                    <th>Sunday</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
+                    {days.map((day, index) => (
+                      <th key={index}>{day}</th>
+                    ))}
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td className="class-time">6:00am - 8:00am</td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Pilates</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="fitness">
-                      <button className="btn btn-bg-none">
-                        <h5>Cardio</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Yoga</h5>
-                        <span>Marcus Chen</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="fitness">
-                      <button className="btn btn-bg-none">
-                        <h5>Circuits</h5>
-                        <span>Alexis Taylor</span>
-                      </button>
-                    </td>
-                    <td className="dark-bg blank-td"></td>
-                    <td className="hover-bg ts-meta" data-tsmeta="motivation">
-                      <button className="btn btn-bg-none">
-                        <h5>Boxing</h5>
-                        <span>Ryan Johnson</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Body Building</h5>
-                        <span>Robert Cage</span>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="class-time">10:00am - 12:00am</td>
-                    <td className="blank-td"></td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="fitness"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Circuits</h5>
-                        <span>Alexis Taylor</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="workout">
-                      <button className="btn btn-bg-none">
-                        <h5>Pilates</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="motivation"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Cardio</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="workout">
-                      <button className="btn btn-bg-none">
-                        <h5>Body Building</h5>
-                        <span>Robert Cage</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="motivation"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Karate</h5>
-                        <span>Samantha Rodriguez</span>
-                      </button>
-                    </td>
-                    <td className="blank-td"></td>
-                  </tr>
-                  <tr>
-                    <td className="class-time">5:00pm - 7:00pm</td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="fitness"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Boxing</h5>
-                        <span>Ryan Johnson</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="motivation">
-                      <button className="btn btn-bg-none">
-                        <h5>Karate</h5>
-                        <span>Samantha Rodriguez</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Body Building</h5>
-                        <span>Robert Cage</span>
-                      </button>
-                    </td>
-                    <td className="blank-td"></td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Yoga</h5>
-                        <span>Marcus Chen</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="motivation">
-                      <button className="btn btn-bg-none">
-                        <h5>Cardio</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="fitness"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Circuits</h5>
-                        <span>Alexis Taylor</span>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="class-time">7:00pm - 9:00pm</td>
-                    <td className="hover-bg ts-meta" data-tsmeta="motivation">
-                      <button className="btn btn-bg-none">
-                        <h5>Cardio</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                    <td className="dark-bg blank-td"></td>
-                    <td className="hover-bg ts-meta" data-tsmeta="fitness">
-                      <button className="btn btn-bg-none">
-                        <h5>Boxing</h5>
-                        <span>Ryan Johnson</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="workout"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Yoga</h5>
-                        <span>Marcus Chen</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="motivation">
-                      <button className="btn btn-bg-none">
-                        <h5>Karate</h5>
-                        <span>Samantha Rodriguez</span>
-                      </button>
-                    </td>
-                    <td
-                      className="dark-bg hover-bg ts-meta"
-                      data-tsmeta="fitness"
-                    >
-                      <button className="btn btn-bg-none">
-                        <h5>Boxing</h5>
-                        <span>Ryan Johnson</span>
-                      </button>
-                    </td>
-                    <td className="hover-bg ts-meta" data-tsmeta="workout">
-                      <button className="btn btn-bg-none">
-                        <h5>Pilates</h5>
-                        <span>Jenna Smith</span>
-                      </button>
-                    </td>
-                  </tr>
+                  {handleSchedule}
+                  {timeSlots.map((timeSlot, timeIndex) => (
+                    <tr key={timeIndex} className="class-time">
+                      <td>{timeSlot}</td>
+                      {workouts.map((workout, workoutIndex) => (
+                        <td key={workoutIndex} className="hover-bg ts-meta">
+                          <button className="btn btn-bg-none">
+                            <h5>{workout.name}</h5>
+                            <span>{workout.trainer}</span>
+                          </button>
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
