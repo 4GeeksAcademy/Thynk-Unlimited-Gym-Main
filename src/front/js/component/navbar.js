a48f4218398946518818239ef18038d8b1589474
Jvonte Walker <107204895+ScRubbishJay@users.noreply.github.com>
2024-03-16 00:23:53 +0000

    navbar new

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,42 +6,32 @@ import "../../styles/navbar-footer.css"
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark navbar-expand-md">
+    <nav className="navbar navbar-dark navbar-expand-md align-items-center">
         {/* Logo */}
         <div className="navbar-brand navbar-left">
           <Link to="/">
             <img className="" src={logo} height={135}  alt="Thynk Unlimited Logo" />
           </Link>
         </div>
-
-        {/* Container for login section and links */}
-        <div className="navbar-nav navbar-right">
-          {/* Login */}
-          <div className="login-section">
-            <form action="" method="post">
-              <label htmlFor="uname">
-                <b>Username</b>
-              </label>
-              <input type="text" placeholder="Enter Username" name="uname" required />
-              <label htmlFor="psw">
-                <b>Password</b>
-              </label>
-              <input type="password" placeholder="Enter Password" name="psw" required />
-              <button type="submit">Login</button>
-            </form>
-          </div>
-
+      
           {/* Links */}
           
             <ul className="navbar-list">
               <li className="navbar-item"><Link to="/">Home</Link></li>
+              
               <li className="navbar-item"><Link to="about">About Us</Link></li>
+              
               <li className="navbar-item"><Link to="/members">Members</Link></li>
+              
               <li className="navbar-item"><Link to="/classes">Classes</Link></li>
+              
               <li className="navbar-item"><Link to="/resources">Resources</Link></li>
+              
               <li className="navbar-item"><Link to="/shop">Shop</Link></li>
+              
+              <li className="navbar-item"><Link to="/login"><button type="button" class="btn btn-outline-warning">Login</button></Link></li>
+              
             </ul>
-          </div>
     </nav>
   );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -28,7 +28,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<AboutUs />} path="/about" />
                         {/* <Route element={<Member />} path="/member" /> */}
-                        <Route element={<Single />} path="/single/:theid" />
+                        
                         <Route element={< MembershipViewPage/> } path="/membershipView/:id"/> 
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
