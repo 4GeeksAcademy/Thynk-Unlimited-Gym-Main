760f9ef2406fd0bab0218d9fe14cec18cab8348d
Jvonte Walker <107204895+ScRubbishJay@users.noreply.github.com>
2024-03-18 23:38:53 +0000

    css done

--- src/front/js/pages/aboutUs.js
+++ src/front/js/pages/aboutUs.js
@@ -1,64 +1,152 @@
 import React from "react";
 import "../../styles/index.css"
+import "../../styles/aboutUs.css"
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 
 export const AboutUs = ()=>{
     return (
-        <div>
+      <div className="about-us container d-flex flex-column justify-content-center align-items-center p-5 my-3">
 
         <div>
-        <h1>Meet the Team</h1>
+        <h1 className="text-center my-5">Meet the Team</h1>
 
-        <div className="d-flex p-4">
+        <div className="d-flex justify-content-between my-3">
 
 
-        <div className="card bg-black">
-        <img src="..." class="card-img-top img-fluid" alt="Placeholder Image"/>
+        <div className="card col-3">
+        <img src="https://d2x313g9lpht1q.cloudfront.net/original/3X/a/c/ac79769d411ef6ca2333fb5b6cb8c2089079de25.jpeg" className ="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
     <p className="card-text">Bio: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <p className="card-text">Role:</p>
+    <p> Socials:<a href="">
+          <FaSquareXTwitter size="2rem" />
+        </a>
+        <a href="">
+          <FaInstagramSquare size="2rem" />
+        </a>
+        <a href="">
+          <FaFacebook size="2rem" />
+          </a>
+        <a href="">
+          <MdEmail size="2rem" />
+        </a> </p>
   </div>
 </div>
-        <div className="card bg-black">
-        <img src="https://via.placeholder.com/200x200" class="card-img-top img-fluid" alt="Placeholder Image"/>
+        <div className="card col-3">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Ja4RMfFECmhvJQ77_B3r60PpbjAyaLB8Qe1ownapIw&s" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
     <p className="card-text">Bio: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <p className="card-text">Role:</p>
+    <p> Socials:<a href="">
+          <FaSquareXTwitter size="2rem" />
+        </a>
+        <a href="">
+          <FaInstagramSquare size="2rem" />
+        </a>
+        <a href="">
+          <FaFacebook size="2rem" />
+          </a>
+        <a href="">
+          <MdEmail size="2rem" />
+        </a></p>
   </div>
 </div>
-        <div className="card bg-black">
-        <img src="https://via.placeholder.com/200x200" class="card-img-top img-fluid" alt="Placeholder Image"/>
+        <div className="card col-3">
+        <img src="https://origympersonaltrainercourses.co.uk/files/img_cache/30647/1920_1659430668_ptmalepose1.png?1659432239" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
     <p className="card-text">Bio: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <p className="card-text">Role:</p>
+    <p> Socials:<a href="">
+          <FaSquareXTwitter size="2rem" />
+        </a>
+        <a href="">
+          <FaInstagramSquare size="2rem" />
+        </a>
+        <a href="">
+          <FaFacebook size="2rem" />
+          </a>
+        <a href="">
+          <MdEmail size="2rem" />
+        </a></p>
   </div>
 </div>
-        <div className="card bg-black">
-        <img src="https://via.placeholder.com/200x200" class="card-img-top img-fluid" alt="Placeholder Image"/>
+        <div className="card col-3">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl2upJG-KyMcv5fFDfeG9SYG1CY9oa9wWMtCA8XXhEsQ&s" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
     <p className="card-text">Bio: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <p className="card-text">Role:</p>
+    <p> Socials:<a href="">
+          <FaSquareXTwitter size="2rem" />
+        </a>
+        <a href="">
+          <FaInstagramSquare size="2rem" />
+        </a>
+        <a href="">
+          <FaFacebook size="2rem" />
+          </a>
+        <a href="">
+          <MdEmail size="2rem" />
+        </a></p>
   </div>
 </div>
-
-</div>
-        
         
         </div>
-
+        </div>
 
         <h2>Our Mission</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, ipsa? At sequi voluptates fuga dolorem rerum iste! Ea deleniti nam sapiente placeat magnam eaque, harum hic expedita cum culpa. Quis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, ipsa? At sequi voluptates fuga dolorem rerum iste! Ea deleniti nam sapiente placeat magnam eaque, harum hic expedita cum culpa. Quis.</p>
+
+        
 
         <h2>Locations</h2>
-
-        <address>123 cardio ave. chicago,IL,60456</address>
-        <p>Phone:</p>
+<div className="d-flex my-3">
+        <div className="my-4">
+        <div className="card">
+        <img src="https://cdn.choosechicago.com/uploads/2022/09/Pink-skyline-with-out-logo.jpeg" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
+  <div className="card-body">
+        <p>Phone:(773) 555-1234</p>
+        <p>13942 Champs. ave. Chicago,IL,60456</p>
+  </div>
+  </div>
+  </div>
+        <div className="my-4">
+        <div className="card">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaZ5EWySydls-1tFG9E41YDDqO6EHoOxmURtBVhfFKgg&s" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
+  <div className="card-body">
+  <p>Phone:(708) 555-5566</p>
+        <p>6329 Cardio ave. Dallas,Tx,75214</p>
+  </div>
+  </div>
+  </div>
+        <div className="my-4">
+        <div className="card">
+        <img src="https://i.natgeofe.com/n/2f26e117-08d9-4364-a76b-817421bfc5d6/denver-travel_4x3.jpg" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
+  <div className="card-body">
+  <p>Phone:(312) 555-6655</p>
+        <p> 311 Creatine blvd. Miami,FL,33102</p>
+  </div>
+</div>
+</div>
+        <div className="my-4">
+        <div className="card">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/View_of_Dallas_from_Reunion_Tower_August_2015_13.jpg/1200px-View_of_Dallas_from_Reunion_Tower_August_2015_13.jpg" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
+  <div className="card-body">
+  <p>Phone:(708) 555-4466</p>
+        <p> 5723 Cramps ave. Denver,CO,80238</p>
+  </div>
+</div>
+</div>
         
+        </div>
         
         </div>
 
