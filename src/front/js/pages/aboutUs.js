450bfe39f4261ce59fbd734ac0ff3bea03b10470
Jvonte Walker <107204895+ScRubbishJay@users.noreply.github.com>
2024-03-19 02:44:20 +0000

    css changes

--- src/front/js/pages/aboutUs.js
+++ src/front/js/pages/aboutUs.js
@@ -22,7 +22,7 @@ export const AboutUs = ()=>{
         <div className="d-flex justify-content-between my-3">
 
 
-        <div className="card col-3">
+        <div className="card card-about col-3">
         <img src="https://d2x313g9lpht1q.cloudfront.net/original/3X/a/c/ac79769d411ef6ca2333fb5b6cb8c2089079de25.jpeg" className ="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
@@ -42,7 +42,7 @@ export const AboutUs = ()=>{
         </a> </p>
   </div>
 </div>
-        <div className="card col-3">
+        <div className="card card-about col-3">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Ja4RMfFECmhvJQ77_B3r60PpbjAyaLB8Qe1ownapIw&s" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
@@ -62,7 +62,7 @@ export const AboutUs = ()=>{
         </a></p>
   </div>
 </div>
-        <div className="card col-3">
+        <div className="card card-about col-3">
         <img src="https://origympersonaltrainercourses.co.uk/files/img_cache/30647/1920_1659430668_ptmalepose1.png?1659432239" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
@@ -82,7 +82,7 @@ export const AboutUs = ()=>{
         </a></p>
   </div>
 </div>
-        <div className="card col-3">
+        <div className="card card-about col-3">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl2upJG-KyMcv5fFDfeG9SYG1CY9oa9wWMtCA8XXhEsQ&s" className="card-img-top img-fluid card-img" alt="Placeholder Image"/>
   <div className="card-body">
     <h5 className="card-title">Name:</h5>
