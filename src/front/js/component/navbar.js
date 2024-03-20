71c6e374229cda4a9a118055cedcca8669345822
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-03-20 23:49:54 +0000

    baseline commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,52 +5,49 @@ import "../../styles/navbar-footer.css";
 
 export const Navbar = () => {
   return (
-
-    <nav className="navbar navbar-expand-md align-items-center">
-      {/* Logo */}
-      <div className="navbar-brand navbar-left">
-        <Link to="/">
-          <img
-            className=""
-            src={logo}
-            height={135}
-            alt="Thynk Unlimited Logo"
-          />
-        </Link>
-      </div>
-
-      {/* Links */}
-
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/about">About Us</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/members">Members</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/classes">Classes</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/resources">Resources</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/shop">Shop</Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link to="/login">
-            <button type="button" className="login-btn btn btn-bg-none">
-              Login
-            </button>
+    <div>
+      <nav className="navbar navbar-expand-md align-items-center">
+        {/* Logo */}
+        <div className="navbar-brand navbar-left">
+          <Link to="/">
+            <img
+              className=""
+              src={logo}
+              height={135}
+              alt="Thynk Unlimited Logo"
+            />
           </Link>
+        </div>
+
+        {/* Links */}
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/members">Members</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/classes">Classes</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/login">
+              <button type="button" className="login-btn btn btn-bg-none">
+                Login
+              </button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
--- src/front/js/pages/resources.js
+++ src/front/js/pages/resources.js
@@ -1,12 +1,8 @@
-import { error } from "jquery";
 import React, { useState, useEffect } from "react";
 import "../../styles/resources.css";
-import { ScaleLoader } from "react-spinners";
-import "animate.css";
 
 function Resources() {
   const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(process.env.BACKEND_URL + "/api/articles")
@@ -15,87 +11,52 @@ function Resources() {
       .catch((error) => console.log(error));
   }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
   return (
     <div>
-
-      {loading ? (
-        <div className="loading-spinner">
-          <ScaleLoader id="loader" color="#e8c552" height={75} width={8} />
+      {/* Healthy LifeStyle Jumbotron*/}
+      <div
+        className="jumbotron jumbotron-fluid"
+        style={{ marginTop: "60px", marginBottom: "30px" }}
+      >
+        <div className="container text-center">
+          <h1 className="display-4">Empowering your journey</h1>
+          <p className="lead fs-4">
+            Inspiring Articles for a Healthier Lifestyle
+          </p>
         </div>
-      ) : (
-        <>
-          {/* Healthy LifeStyle Jumbotron*/}
+      </div>
 
-          <div
-            className="jumbotron jumbotron fluid animate__animated animate__fadeInUp"
-            style={{
-              marginTop: "60px",
-              marginBottom: "30px",
-            }}
-          >
-            <div className="container text-center">
-              <h1 className="display-4">Empowering your journey</h1>
-              <p className="lead fs-4">
-                Inspiring Articles for a Healthier Lifestyle
-              </p>
-            </div>
-          </div>
-
-          {/* Article Cards */}
-          <div
-            className="container animate__animated animate__slideInUp"
-            style={{
-              maxWidth: "100%",
-              marginLeft: "60px",
-              marginRight: "60px",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-between",
-              }}
-            >
-              {articles.map((article, index) => (
-                <div
-                  className="card text-black"
-                  key={index}
-                  style={{
-                    width: "calc(33.33% - 20px)",
-                    marginBottom: index < 3 ? "60px" : "60px", // Different margins for the first and second row
-                    marginTop: index >= 3 ? "0px" : "80px", // Different top margin for the second row
-                    backgroundColor: "#E8C552", // Background color of the cards
-                    padding: "20px", // Add padding for better spacing
-                  }}
-                >
-                  <img
-                    src={article.image_url}
-                    className="card-img-top"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{article.title}</h5>
-                    <p className="card-text"> {article.preview}</p>
-                    <a href={article.button_url} className="btn btn-dark">
-                      Read More
-                    </a>
-                  </div>
+      {/* Article Cards */}
+      <div
+        className="container"
+        style={{ maxWidth: "100%", marginLeft: "60px", marginRight: "60px" }}
+      >
+        <div className="row justify-content-between">
+          {articles.map((article, index) => (
+            <div className="col-md-4 mb-4" key={index}>
+              <div
+                className="card text-black"
+                style={{ backgroundColor: "#E8C552", padding: "20px" }}
+              >
+                <img
+                  src={article.image_url}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{article.title}</h5>
+                  <p className="card-text">{article.preview}</p>
+                  <a href={article.button_url} className="btn btn-dark">
+                    Read More
+                  </a>
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
-        </>
-      )}
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Resources;
-
